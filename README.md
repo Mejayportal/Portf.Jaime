299dd4e7b952cd808557488d7ce42c0d45a8b5ab
Mejayportal <117112083+Mejayportal@users.noreply.github.com>
2023-07-02 23:58:51 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Portf.Jaime
+# Jayme
+porfolio
---
+++ script.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+
+  $('#menu').click(function(){
+    $(this).toggleClass('fa-times');
+    $('header').toggleClass('toggle');
+  });
+
+  $(window).on('scroll load',function(){
+
+    $('#menu').removeClass('fa-times');
+    $('header').removeClass('toggle');
+
+    if($(window).scrollTop() > 0){
+      $('.top').show();
+    }else{
+      $('.top').hide();
+    }
+
+  });
+
+  // smooth scrolling 
+
+  $('a[href*="#"]').on('click',function(e){
+
+    e.preventDefault();
+
+    $('html, body').animate({
+
+      scrollTop : $($(this).attr('href')).offset().top,
+
+    },
+      800, 
+      'linear'
+    );
+
+  });
+
+  // <!-- text-animation code link -->
+  
+  var typed = new Typed('#element', {
+    strings: ['Web Developer.','Wordpress Developer.','Video/Photo Editor.'],
+    typeSpeed: 100,
+    backSpeed:100,
+    loop: true
+  });
+
+});
+
+
